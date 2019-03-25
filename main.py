dd38c3fc7a46ad3a0777bf91d2daa5bbea781bbc
lmurphy95 <lmurp14@u.rochester.edu>
2019-03-25 11:04:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+import pymysql
+
+
+name = ""
+password = ""
+rds_host = ""
+db_name = ""
+port = ""
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name,
+                           passwd=password, db=db_name, connect_timeout=5)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
+
+def lambda_handler(event, context):
+    return ""
